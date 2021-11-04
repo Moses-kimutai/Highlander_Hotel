7a35f4be9605fd77bcffc9243e9c0fe8d52352e9
Moses_Kimutai <jamalmudi546@gmail.com>
2021-11-04 19:50:40 -0400

    Add project files.

---
+++ Highlander_Hotel/Form1.Designer.vb
@@ -0,0 +1,369 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.LblTotal = New System.Windows.Forms.Label()
+        Me.LblTax = New System.Windows.Forms.Label()
+        Me.LblSub = New System.Windows.Forms.Label()
+        Me.LblAdditional = New System.Windows.Forms.Label()
+        Me.LblRoom = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 85)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(489, 197)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Room Information."
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(152, 112)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(239, 27)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(147, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(244, 27)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nightly Charge:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nights:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox2.Controls.Add(Me.TextBox5)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox2.Location = New System.Drawing.Point(520, 85)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(496, 197)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Additional Charges."
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(178, 130)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(217, 27)
+        Me.TextBox5.TabIndex = 5
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(178, 82)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(217, 27)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(178, 35)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(217, 27)
+        Me.TextBox3.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(17, 137)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 20)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Misc:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Telephone:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Room Service:"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox3.Controls.Add(Me.LblTotal)
+        Me.GroupBox3.Controls.Add(Me.LblTax)
+        Me.GroupBox3.Controls.Add(Me.LblSub)
+        Me.GroupBox3.Controls.Add(Me.LblAdditional)
+        Me.GroupBox3.Controls.Add(Me.LblRoom)
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox3.Location = New System.Drawing.Point(25, 300)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(991, 332)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Total Charges."
+        '
+        'LblTotal
+        '
+        Me.LblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblTotal.Location = New System.Drawing.Point(572, 290)
+        Me.LblTotal.Name = "LblTotal"
+        Me.LblTotal.Size = New System.Drawing.Size(213, 25)
+        Me.LblTotal.TabIndex = 9
+        '
+        'LblTax
+        '
+        Me.LblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblTax.Location = New System.Drawing.Point(572, 236)
+        Me.LblTax.Name = "LblTax"
+        Me.LblTax.Size = New System.Drawing.Size(213, 25)
+        Me.LblTax.TabIndex = 8
+        '
+        'LblSub
+        '
+        Me.LblSub.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblSub.Location = New System.Drawing.Point(572, 182)
+        Me.LblSub.Name = "LblSub"
+        Me.LblSub.Size = New System.Drawing.Size(213, 25)
+        Me.LblSub.TabIndex = 7
+        '
+        'LblAdditional
+        '
+        Me.LblAdditional.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblAdditional.Location = New System.Drawing.Point(572, 124)
+        Me.LblAdditional.Name = "LblAdditional"
+        Me.LblAdditional.Size = New System.Drawing.Size(213, 25)
+        Me.LblAdditional.TabIndex = 6
+        '
+        'LblRoom
+        '
+        Me.LblRoom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblRoom.Location = New System.Drawing.Point(572, 74)
+        Me.LblRoom.Name = "LblRoom"
+        Me.LblRoom.Size = New System.Drawing.Size(213, 25)
+        Me.LblRoom.TabIndex = 5
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(152, 296)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(108, 20)
+        Me.Label10.TabIndex = 4
+        Me.Label10.Text = "Total Charges:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(152, 242)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 20)
+        Me.Label9.TabIndex = 3
+        Me.Label9.Text = "Tax:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(152, 188)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(72, 20)
+        Me.Label8.TabIndex = 2
+        Me.Label8.Text = "Subtotal:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(152, 129)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(146, 20)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Additional Charges:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(152, 74)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(115, 20)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Room Charges:"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(76, 665)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(154, 59)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "CALCULATE CHARGES"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(433, 666)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(126, 58)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "CLEAR"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(771, 665)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 59)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "EXIT"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label11
+        '
+        Me.Label11.BackColor = System.Drawing.SystemColors.Info
+        Me.Label11.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label11.Location = New System.Drawing.Point(382, 23)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(220, 42)
+        Me.Label11.TabIndex = 6
+        Me.Label11.Text = "Highlander Hotel."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1074, 740)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LblTotal As Label
+    Friend WithEvents LblTax As Label
+    Friend WithEvents LblSub As Label
+    Friend WithEvents LblAdditional As Label
+    Friend WithEvents LblRoom As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label11 As Label
+End Class
---
+++ Highlander_Hotel/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' Declaration of variables'
+        Dim Nights As Double
+        Dim Night_Charge As Double
+        Dim Room_Service As Double
+        Dim Telephone As Double
+        Dim Misc As Double
+        Dim Room_Charges As Double
+        Dim Additional As Double
+        Dim Subtotal As Double
+        Dim Tax As Double
+        Dim Total_Charges As Double
+
+
+        ' this allows textbox inputs to be transfered to spesific variable'
+
+        'try checks if the inputs are double if not a pop up message will apear asking you to enter a double number'
+        Try
+            Nights = Val(TextBox1.Text)
+            Night_Charge = Val(TextBox2.Text)
+            Room_Service = Val(TextBox3.Text)
+            Telephone = Val(TextBox4.Text)
+            Misc = Val(TextBox5.Text)
+
+        Catch
+            MessageBox.Show(" Enter a double number.")
+
+        End Try
+
+        'calculation of  Room charges, Additional charges, Tubtotal, Tax and Total charges.'
+        Room_Charges = Nights * Night_Charge
+        Additional = Room_Charges + Room_Service + Misc
+        Subtotal = Room_Charges + Additional
+        Tax = Subtotal * 0.07
+        Total_Charges = Subtotal + Tax
+
+        'conversion of double variables to string'
+        'displaying the calculations in the labels'
+        LblRoom.Text = ("$") & Room_Charges.ToString("n2")
+        LblAdditional.Text = ("$") & Additional.ToString("n2")
+        LblSub.Text = ("$") & Subtotal.ToString("n2")
+        LblTax.Text = ("$") & Tax.ToString("n2")
+        LblTotal.Text = ("$") & Total_Charges.ToString("n2")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'clears all the textboxes and display labels.'
+        TextBox1.ResetText()
+        TextBox2.ResetText()
+        TextBox3.ResetText()
+        TextBox4.ResetText()
+        TextBox5.ResetText()
+        LblRoom.ResetText()
+        LblAdditional.ResetText()
+        LblSub.ResetText()
+        LblTax.ResetText()
+        LblTotal.ResetText()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Closes the application'
+        Me.Close()
+
+    End Sub
+End Class
